6ba6c7835ee23c74033b941bb8cecbb1fb0d1ad2
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-10 10:54:31 +0200

    Add project files.

---
+++ Dog.cs
@@ -0,0 +1,12 @@
+﻿namespace LINQ240910
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public bool Sex { get; set; }
+        public string Breed { get; set; }
+        public int Age => DateTime.Now.Year - Birth.Year;
+        public float Weight { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿#region DOGS
+using LINQ240910;
+
+List<Dog> dogs = new()
+{
+    new() //01
+    {
+        Name = "Kira",
+        Birth = DateTime.Parse("2019-05-10"),
+        Sex = false,
+        Breed = "tacskó",
+        Weight = 5.5F,
+    },
+    new() //02
+    {
+        Name = "Rex",
+        Birth = DateTime.Parse("2014-04-20"),
+        Sex = true,
+        Breed = "németjuhász",
+        Weight = 35.2F,
+    },
+    new() //03
+    {
+        Name = "Igor",
+        Birth = DateTime.Parse("2017-11-20"),
+        Sex = false,
+        Breed = "kaukázusi farkasölő",
+        Weight = 95.0F,
+    },
+    new() //04
+    {
+        Name = "Thomas Edison",
+        Birth = DateTime.Parse("2001-02-07"),
+        Sex = false,
+        Breed = "utcamix",
+        Weight = 40.3F,
+    },
+    new() //05
+    {
+        Name = "Princess",
+        Birth = DateTime.Parse("2022-12-24"),
+        Sex = false,
+        Breed = "palotapincsi",
+        Weight = 4.2F,
+    },
+};
+#endregion
